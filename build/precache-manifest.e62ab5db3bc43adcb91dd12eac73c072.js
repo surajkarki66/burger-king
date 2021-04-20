a6d055fedf4fb090b2234156f4b096dff2bec399
surajkarki66 <suraj.karki500@protonmail.com>
2021-04-20 17:23:24 +0545

    Fixes minor bugs

---
+++ build/precache-manifest.e62ab5db3bc43adcb91dd12eac73c072.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccd27b07eb602f246940fa496697e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96421df85a0205982f33",
+    "url": "/static/css/main.cb04ac8a.chunk.css"
+  },
+  {
+    "revision": "02fdf965e2f62e21cfe5",
+    "url": "/static/js/2.c1bcb773.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c1bcb773.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96421df85a0205982f33",
+    "url": "/static/js/main.ab2d7e76.chunk.js"
+  },
+  {
+    "revision": "fa4f892f4cbfdb1cef12",
+    "url": "/static/js/runtime-main.99e33ab9.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
